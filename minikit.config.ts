1708df8438e25140c0f46af3d98377711ab22136
satosh <105781795+satoshik505@users.noreply.github.com>
2025-12-03 16:47:11 +0100

    Create minikit.config.ts

---
+++ minikit.config.ts
@@ -0,0 +1,26 @@
+export const minikitConfig = {
+  accountAssociation: { // this will be added in step 5
+    "header": "",
+    "payload": "",
+    "signature": ""
+  },
+  miniapp: {
+    version: "1",
+    name: "Cubey", 
+    subtitle: "Your AI Ad Companion", 
+    description: "Ads",
+    screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
+    iconUrl: `${ROOT_URL}/blue-icon.png`,
+    splashImageUrl: `${ROOT_URL}/blue-hero.png`,
+    splashBackgroundColor: "#000000",
+    homeUrl: ROOT_URL,
+    webhookUrl: `${ROOT_URL}/api/webhook`,
+    primaryCategory: "social",
+    tags: ["marketing", "ads", "quickstart", "waitlist"],
+    heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
+    tagline: "",
+    ogTitle: "",
+    ogDescription: "",
+    ogImageUrl: `${ROOT_URL}/blue-hero.png`,
+  },
+} as const;
